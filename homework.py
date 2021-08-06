ebd304d8bf8982e4062abb025a0fd0c4c98e71a8
ggerasyanov <QieDie@yandex.ru>
2021-08-06 12:46:40 +0300

    Доработать

--- homework.py
+++ homework.py
@@ -4,7 +4,7 @@ import datetime as dt
 class Calculator:
     def __init__(self, limit):
         self.limit = limit
-        self.record = record[]
+        self.records = []
 
 
 class Record:
@@ -13,7 +13,7 @@ class Record:
         self.amount = amount
         self.date = date
         self.comment = comment
-      
+
 
 class CashCalculator(Calculator):
     def __init__(self, limit, record, currency):
@@ -21,11 +21,41 @@ class CashCalculator(Calculator):
         self.currency = currency
 
     def add_record(self, add_record):
-        record.append(add_record)
+        records.append(add_record)
 
-    def get_today_stats(self, currency = 'rub'):
+    def get_today_stats(self):
+        now = dt.datetime.now()
+        sym_amount = 0
+        for date in record:
+            if date.date == now.date():
+                sym_amount += date.amount
+        return sym_amount
+                
+    def get_today_cash_remained(self, currency='rub'): # <--- Закончил тут! Делаю функцию для подставки валюты.
+        USD_RATE = 70
+        EURO_RATE = 85
+        limit = self.limit
+        sym_amount = self.get_today_stats()
+        left_money = (limit - sym_amount) / USD_RATE
+        def response(currency):
+            views_currency = ['руб', 'USD', 'EURO']
+            if left_money == 0:
+                print('Денег нет, держись')
+            elif left_money < 0:
+                print(f'Денег нет, держись: твой долг - {left_money} {views_currency[]}')
+            else:
+                print(f'На сегодня осталось {left_money} USD')
 
-    def get_today_cash_remained(self):
+        if currency == 'usd':
+            if left_money == 0:
+                print('Денег нет, держись')
+            elif left_money < 0:
+                print(f'Денег нет, держись: твой долг - {left_money} USD')
+            else:
+                print(f'На сегодня осталось {left_money} USD')
+        elif currency == 'eur:':
+            
+
 
     def get_week_stats(self):
 
