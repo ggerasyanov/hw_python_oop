d96dab5602f079974ccb822bfe29dc468ff56929
ggerasyanov <QieDie@yandex.ru>
2021-08-06 12:42:38 +0300

    Доработать

--- homework.py
+++ homework.py
@@ -2,8 +2,45 @@ import datetime as dt
 
 
 class Calculator:
-    pass
+    def __init__(self, limit):
+        self.limit = limit
+        self.record = record[]
 
 
 class Record:
-    pass
+    now = dt.datetime.now()
+    def __init__(self, amount, comment, date=now.date()):
+        self.amount = amount
+        self.date = date
+        self.comment = comment
+      
+
+class CashCalculator(Calculator):
+    def __init__(self, limit, record, currency):
+        super().__init__(limit, record)
+        self.currency = currency
+
+    def add_record(self, add_record):
+        record.append(add_record)
+
+    def get_today_stats(self, currency = 'rub'):
+
+    def get_today_cash_remained(self):
+
+    def get_week_stats(self):
+
+
+class CaloriesCalculator(Calculator):
+    def __init__(self, limit, record):
+        super().__init__(limit, record)
+    
+    def add_record(self):
+
+    def get_today_stats(self):
+
+    def get_calories_remained(self):
+
+    def get_week_stats(self):
+
+cash_calculator.add_record(Record(amount=145, comment='кофе'))
+
